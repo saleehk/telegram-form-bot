cd7a7c231d0ba8bc6450882c604e8a5877c9c3d6
saleeh <saleeh93@gmail.com>
2018-05-30 14:02:34 +0530

    first commit

---
+++ bot.js
@@ -0,0 +1,73 @@
+'use strict'
+
+const Telegram = require('telegram-node-bot');
+const TelegramBaseController = Telegram.TelegramBaseController;
+const TextCommand = Telegram.TextCommand;
+const tg = new Telegram.Telegram('TOKEN');
+
+class PingController extends TelegramBaseController {
+    /**
+     * @param {Scope} $
+     */
+    pingHandler($) {
+        const form = {
+            name: {
+                q: 'Send me your name',
+                error: 'sorry, wrong input',
+                validator: (message, callback) => {
+
+                    if (message.text) {
+                        callback(true, message.text) //you must pass the result also
+                        return
+                    }
+
+                    callback(false)
+                }
+            },
+            age: {
+                q: 'Send me your age',
+                error: 'sorry, wrong input',
+                validator: (message, callback) => {
+
+                    if (message.text=="hello") {
+                        callback(true, message.text) //you must pass the result also
+                        return
+                    }
+
+                    callback(false)
+                }
+            },
+            email: {
+                q: 'Send me your Email',
+                error: 'sorry, wrong input',
+                validator: (message, callback) => {
+
+                    if (message.text) {
+                        callback(true, message.text) //you must pass the result also
+                        return
+                    }
+
+                    callback(false)
+                }
+            }
+        }
+
+        $.runForm(form, (result) => {
+             $.sendMessage(` ${result.name},${result.age} ,${result.email}  `)
+            console.log(result)
+        })
+        // $.sendMessage('pong')
+    }
+
+    get routes() {
+        return {
+            'pingCommand': 'pingHandler'
+        }
+    }
+}
+
+tg.router
+    .when(
+        new TextCommand('/start', 'pingCommand'),
+        new PingController()
+    );
